3f712f55b3b0b38d64e7ddadad599a0525d8b5bf
Timothy A. V. Teatro <timtro@localhost.localdomain>
2014-11-18 09:58:27 -0500

    Now working Dth controller.

--- pyNMPC/modules/nmpc_funcs.py
+++ pyNMPC/modules/nmpc_funcs.py
@@ -6,15 +6,21 @@ dxymask = np.array([False, True, False, True, False])
 def airline_path( x0, vref, xref, T, tgt):
 	direction = (tgt - x0[xymask])
 	direction = direction.reshape(2,1) / np.sqrt(2 * np.sum(direction**2)) # The factor of 1/sqrt(2) belons to vref, but is put here to reduce computation
-	xref[xymask,:] = vref*T*direction
-	xref[xymask,:] = x0[xymask].reshape(2,1) + np.cumsum(xref[xymask,:], axis=1)
+	xref[xymask, :] = vref*T*direction * np.sqrt(2)
+	xref[xymask, 0] += x0[xymask]
+	xref[xymask, :] = np.cumsum(xref[xymask,:], axis=1)
 
-def f(x, u, Phi, obst, T, eps, n):
+def f(x, u, v, Phi, obst, T, eps, n):
 	'''
 	Based on the velocity values in u (we control velocity), this function uses
 	Euler integration to predict x over the horizon of N points.
 	'''
-	x[:, 1:] = x[:, 0].reshape(n,1) + np.cumsum(u, axis=1) * T
+	for k in range(1, x.shape[-1]):
+		x[4, k] = x[4, k-1] + u[0, k-1] * T
+		x[1, k] = v[k-1] * np.sin(x[4, k])
+		x[3, k] = v[k-1] * np.cos(x[4, k])
+		x[xymask, k] = x[xymask, k-1] + x[dxymask, k-1] * T
+
 	Phi[:,:] = 0
 	for obs in obst.T:
 		dx = x[xymask,1:]-obs.reshape(2, 1)
@@ -25,21 +31,18 @@ def lagrange_mult(x, v, ex, Phi, Q, Q0, T):
 	Ugly lagrange thingy
 	'''
 	# TODO Must I ocmpute Phi[:, -1]?
-	lp = np.ones( ex.shape )
-	lp[4,:] = np.cumsum(lp[4,:])
-	# Old lp from dxy control:
-	# lp[:, -1] = ex[:, -1] * Q0
-	# lp[:, :-1] = ex[:, :-1] * Q  - Phi[:, :-1]
-	# return lp + np.cumsum(lp[:, ::-1], axis=1)[:, ::-1]
+	lp = np.zeros( x.shape )
 
 	lp[xymask, -1] = ex[xymask, -1] * Q0
-	lp[xymask, :-1] = ex[xymask, :-1] * Q  - Phi[:, :-1]
-	lp[dxymask, :-1] = (np.cumsum( lp[dxymask, ::-1], axis=1)[:, ::-1] * T)[:,1:]
-	lp[4, : ] = -np.cumsum(lp[1, ::-1])[::-1] * np.sin(x[4, 1:]) * v[:] \
-		+ np.cumsum(lp[3, ::-1])[::-1] * np.cos(x[4, 1:]) * v[:]\
-		+ np.cumsum(lp[4, ::-1])[::-1]
-	return lp
 
+	for k in reversed(range(lp.shape[-1]-1)):
+		lp[xymask, k] = lp[xymask, k+1] + ex[xymask, k] * Q  - Phi[:, k]
+		lp[dxymask, k] = T * lp[xymask, k+1]
+		lp[4, k] = -lp[1, k+1]*np.sin(x[4,k])*v[k] + lp[3, k+1]*np.cos(x[4,k])*v[k] \
+			+ lp[4, k+1]
+		# lp[4, k] = lp[dxymask, k+1].dot(np.array([-np.sin(x[4,k]), np.cos(x[4,k])]))* v[k] \
+		# 	+ lp[4, k+1]
+	return lp
 
 def grad(u, p, vref, T, R):
 	'''
@@ -47,4 +50,4 @@ def grad(u, p, vref, T, R):
 	'''
 	#v = np.sqrt(np.sum(u**2, axis=0))
 	# print(v)
-	return T*p - R * u
+	return T*p[4, 1:] - R * u
--- pyNMPC/pyNMPC.py
+++ pyNMPC/pyNMPC.py
@@ -5,27 +5,26 @@ import time
 
 from modules import nmpc_funcs as nmpc
 
-T = 0.012 # Time sample coeff.
+T = 0.03 # Time sample coeff.
 n = 5 # state space dimension
 m = 1 # control space dimension
-N = 100 # Prediction Horizon size
+N = 80 # Prediction Horizon size
 Q = 110 # Weighting param
 Q0 = N*1 # Weighting param
-R = 5 # Weighting param
+R = .5 # Weighting param
 mu = 0.02
 eps = .02
-obstacles = np.array([[.15],[.15]])
+obstacles = np.array([[5],[5]])
 waypts = np.array([[10],[10]])
-vref=0.4*np.ones(N-1)
-H=12
+vref = 0.4 * np.ones(N-1)
+H = 4
 
 x = np.zeros( (n, N) )
-u = np.ones( (n, N-1) ) * vref/np.sqrt(2)
-u[1,:] = -u[1,:]
+u = np.random.randn( N-1 ).reshape(1,N-1)
 xref = np.zeros( (n, N-1) )
 p = np.zeros( (n, N-1) )
 Phi = np.zeros( (2, N-1) )
-current_grad = np.ones( (n, N-1) )
+current_grad = np.ones( (m, N-1) )
 
 nmpc.airline_path(x[:,0], vref, xref, T, waypts[:, 0])
 
@@ -35,7 +34,7 @@ xymask = np.array([True, False, True, False, False])
 # construct the while loop to do a convergence test.
 
 plt.ion()
-nmpc.f(x, u, Phi, obstacles, T, eps, n)
+nmpc.f(x, u, vref, Phi, obstacles, T, eps, n)
 refline, = plt.plot(xref[0, :], xref[1, :], 'r.')
 line, = plt.plot(x[0,:], x[1,:], 'b-', marker='.')
 obplot, = plt.plot(obstacles[0,:], obstacles[1,:], 'ko', ms = 8)
@@ -44,46 +43,48 @@ obplot, = plt.plot(obstacles[0,:], obstacles[1,:], 'ko', ms = 8)
 
 while np.sqrt(np.sum((x[xymask, 0] - waypts[:, 0])**2)) > .1:
 
-	current_grad = np.ones( (n, N-1) )
+	current_grad = np.ones( (m, N-1) )
 
-	nmpc.f(x, u, Phi, obstacles, T, eps, n)
-	nmpc.airline_path(x[:,0], vref, xref, T, waypts[:, 0])
+	nmpc.f(x, u, vref, Phi, obstacles, T, eps, n)
+	nmpc.airline_path(x[:, 0], vref, xref, T, waypts[:, 0])
 
-	# line.set_xdata(x[0, :])
-	# line.set_ydata(x[1, :])
-	# refline.set_xdata(xref[0, :])
-	# refline.set_ydata(xref[1, :])
-	# ax = plt.gca()
-	# ax.relim()
-	# ax.autoscale_view()
+	line.set_xdata(x[0, :])
+	line.set_ydata(x[2, :])
+	refline.set_xdata(xref[0, :])
+	refline.set_ydata(xref[2, :])
+	ax = plt.gca()
+	ax.relim()
+	ax.autoscale_view()
+	ax.set_aspect('equal')
+	plt.draw()
 
 	plt.draw()
 
 	while True:
-		nmpc.f(x, u, Phi, obstacles, T, eps, n)
-		ex = x[:, 1:]-xref
+		nmpc.f(x, u, vref, Phi, obstacles, T, eps, n)
+		ex = x[:, 1:] - xref
 		p = nmpc.lagrange_mult(x, vref, ex, Phi, Q, Q0, T)
 		last_grad = current_grad.copy()
-		current_grad = nmpc.grad(u,p, vref, T, R)
+		current_grad = nmpc.grad(u, p, vref, T, R)
 
-		line.set_xdata(x[0, :])
-		line.set_ydata(x[1, :])
-		refline.set_xdata(xref[0, :])
-		refline.set_ydata(xref[1, :])
-		ax = plt.gca()
-		ax.relim()
-		ax.autoscale_view()
-		plt.draw()
+		# line.set_xdata(x[0, :])
+		# line.set_ydata(x[2, :])
+		# refline.set_xdata(xref[0, :])
+		# refline.set_ydata(xref[2, :])
+		# ax = plt.gca()
+		# ax.relim()
+		# ax.autoscale_view()
+		# ax.set_aspect('equal')
+		# plt.draw()
 
 		if np.sum(current_grad**2) < 0.001:
 			break
 
-		u -= mu*current_grad
-
 		if (np.sum(current_grad*last_grad)) >= 0:
 			mu *= 1.02
+			u += mu*current_grad
 		else:
-			u += mu*last_grad
+			u -= mu*last_grad
 			mu /= 3
 
 	# Execute H steps (or simulate by replacing the first element in x)

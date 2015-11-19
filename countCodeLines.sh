26210cbe8af963f10ea6d80528f84a6c14019a94
Timtro <timtro@gmail.com>
2015-11-19 18:10:03 -0500

    Now records counted lines in history with timestamp

--- countCodeLines.sh
+++ countCodeLines.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 cloc --exclude-list-file=countCodeExclude.txt  src/ tests/ tools/
+
+count=`cloc --exclude-list-file=countCodeExclude.txt  src/ tests/ tools/ | awk '{if ($1=="SUM:") print $5}'`
+echo
+echo "TOTAL Line Count: $count"
+
+echo "`date` $count" >> codeLineCountHistory.dat
